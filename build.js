4618f9f686d7744ac793e1187c22d4932c699100
Leon Sorokin <leeoniya@gmail.com>
2019-06-29 15:50:52 -0500

    add cli opts, build 12-col gapless grid dist

--- build.js
+++ build.js
@@ -1,28 +1,46 @@
+// node build.js --cols 12 --breaks xs:320 sm:375 md:768 lg:1024 xl:1280 xxl:1920 --gaps 8 16 24 32 48 --out ./dist/flecks.css
+
+const { generate } = require('./src/flecks.js');
+
 const fs = require('fs');
 
 const cssobj_core = require('cssobj-core');
 const genCss = require('cssobj-plugin-gencss');
 const defaultUnit = require('cssobj-plugin-default-unit');
 
+let CleanCSS = require('clean-css');
+
+const argv = require('yargs-parser')(process.argv.slice(2), {array: ['breaks','gaps']});
+
+if (argv.breaks) {
+	let o = {};
+	argv.breaks.forEach(b => {
+		let [bp, px] = b.split(':');
+		o[bp] = +px;
+	});
+	argv.breaks = o;
+}
+
+if (!argv.out)
+	argv.out = './dist/flecks.css'
+
 const cssobj = cssobj_core({
-  plugins: [
-	defaultUnit('px'),
-    genCss({indent: '  ', newLine: '\n'}),
-  ]
+	plugins: [
+		defaultUnit('px'),
+		genCss({indent: '  ', newLine: '\n'}),
+	]
 });
 
-const { flecks } = require('./src/flecks.js');
+let cssStruct = generate({
+	cols: argv.cols,
+	breaks: argv.breaks,
+	gaps: argv.gaps,
+});
 
-let compiled = cssobj(flecks).css;
+let css = cssobj(cssStruct).css;
 
-fs.writeFileSync('./dist/flecks.css', compiled, 'utf8');
+fs.writeFileSync(argv.out, css, 'utf8');
 
-let CleanCSS = require('clean-css');
-let options = {
-//	format: 'beautify',
-	level: 2,
-};
+let cssMin = new CleanCSS({level: 2}).minify(css).styles;
 
-let output = new CleanCSS(options).minify(compiled);
-
-fs.writeFileSync('./dist/flecks.min.css', output.styles, 'utf8');
+fs.writeFileSync(argv.out.replace('.css', '.min.css'), cssMin, 'utf8');
--- src/flecks.js
+++ src/flecks.js
@@ -1,160 +1,76 @@
-const COLS = [1,2,3,4,5,6,7,8,9,10,11,12];
+function generate(opts) {
+	const COLS = Array(opts.cols || 12).fill(1).map((x, y) => x + y);
+	const BREAKS = opts.breaks || {
+		xs: 320,
+		sm: 375,
+		md: 768,
+		lg: 1024,
+		xl: 1280,
+		xxl: 1920,
+	};
+	const GAPS = opts.gaps || [];
 
-const GAPS = [8, 16, 24, 32, 48];
+	function calc(frac, gap) {
+		var pct = +(frac * 100).toFixed(6);
 
-const WIDTHS = {
-	xs: 320,
-	sm: 375,		// 360?
-//	ms: 412,		// S9+ (iphones are 414)
-//	ms: 667,		// useless breakpoint, no one browses with 667x
-	md: 768,
-	lg: 1024,
-	xl: 1280,
-	xxl: 1920,		// 1600
-};
+		if (gap == 0)
+			return pct + "%";
 
-function calc(frac, gap) {
-	var pct = +(frac * 100).toFixed(6);
+		return "calc(" + pct + "% - " + gap + "px)";
+	}
 
-	if (gap == 0)
-		return pct + "%";
+	const CSS = {
+		".fl": {
+			display: "flex",
+			flexFlow: "row wrap",
+			justifyContent: "center",
 
-	return "calc(" + pct + "% - " + gap + "px)";
-}
+			// * is considered "slow" :/
+			"> *": {
+				//	1. mobile-first: by default, all flex children are full width
+				width: "100%",
+				//	2a. .fi-<col>* flex children should not grow, so it's disabled here to avoid having to disable it everywhere
+				flex: "0 1 auto",
+			},
 
-const FLECKS = {
-	".fl": {
-		display: "flex",
-		flexFlow: "row wrap",
-		justifyContent: "center",
-
-		// * is considered "slow" :/
-		"> *": {
-			//	1. mobile-first: by default, all flex children are full width
-			width: "100%",
-			//	2a. .fi-<col>* flex children should not grow, so it's disabled here to avoid having to disable it everywhere
-			flex: "0 1 auto",
+			// 3. prevent image flex items from being stretched vertically
+			"> img": {
+				alignSelf: "center"
+			}
 		},
-
-		// 3. prevent image flex items from being stretched vertically
-		"> img": {
-			alignSelf: "center"
-		}
-	},
-};
-
-
-/* 2b. re-enable it for flex children without col specifiers */
-
-FLECKS[".fi"] = {
-	width: "auto",
-};
-
-let flexGrow1 = [".fi"];
-let displayNone = [];
-
-for (let bp in WIDTHS) {
-	flexGrow1.push(".fi-" + bp);
-	displayNone.push(".v-" + bp);
-}
-
-FLECKS[flexGrow1.join(",")] = {
-	flexGrow: 1,
-};
-
-FLECKS[displayNone.join(",")] = {
-	display: "none",
-};
-
-COLS.forEach(col => {
-	FLECKS[".fi-" + col] = {
-		width: calc(col/COLS.length, 0),
-	};
-});
-
-COLS.forEach(col => {
-	FLECKS[".o-" + col] = {
-		order: col,
-	};
-});
-
-GAPS.forEach((g, i) => {
-	let gnum = i + 1;
-	let flg = ".fl.g" + gnum;
-	let flgx = ".fl.g" + gnum + "x";
-	let flgy = ".fl.g" + gnum + "y";
-
-	FLECKS[flg] = {
-		padding: g/2,
 	};
 
-	FLECKS[flgx] = {
-		paddingLeft: g/2,
-		paddingRight: g/2,
-	};
 
-	FLECKS[flgy] = {
-		paddingTop: g/2,
-		paddingBottom: g/2,
-	};
+	/* 2b. re-enable it for flex children without col specifiers */
 
-	FLECKS[flg + " > *"] = {
-		margin: g/2,
-	};
-
-	FLECKS[flgx + " > *"] = {
-		marginLeft: g/2,
-		marginRight: g/2,
-	};
-
-	FLECKS[flgy + " > *"] = {
-		marginTop: g/2,
-		marginBottom: g/2,
-	};
-
-	COLS.forEach(col => {
-		let sels = (
-			flg + " > .fi-" + col
-			+ "," +
-			flgx + " > .fi-" + col
-		);
-
-		FLECKS[sels] = {
-			width: calc(col/COLS.length, g),
-		};
-	});
-});
-
-for (let bp in WIDTHS) {
-	let mq = {};
-
-	// hidden at bp+
-	mq[".h-" + bp] = {
-		display: "none",
-	};
-
-	// visible at bp+
-	mq[".v-" + bp] = {
-		display: "inline-block",
-	};
-
-	// width snapping to bp
-	mq[".bp"] = {
-		maxWidth: WIDTHS[bp] + "px"
-	};
-
-	mq[".fi-" + bp] = {
+	CSS[".fi"] = {
 		width: "auto",
 	};
 
+	let flexGrow1 = [".fi"];
+	let displayNone = [];
+
+	for (let bp in BREAKS) {
+		flexGrow1.push(".fi-" + bp);
+		displayNone.push(".v-" + bp);
+	}
+
+	CSS[flexGrow1.join(",")] = {
+		flexGrow: 1,
+	};
+
+	CSS[displayNone.join(",")] = {
+		display: "none",
+	};
+
 	COLS.forEach(col => {
-		mq[".fi-" + col + "-" + bp] = {
+		CSS[".fi-" + col] = {
 			width: calc(col/COLS.length, 0),
 		};
 	});
 
 	COLS.forEach(col => {
-		mq[".o-" + col + "-" + bp] = {
+		CSS[".o-" + col] = {
 			order: col,
 		};
 	});
@@ -165,57 +81,141 @@ for (let bp in WIDTHS) {
 		let flgx = ".fl.g" + gnum + "x";
 		let flgy = ".fl.g" + gnum + "y";
 
-		mq[flg + "-" + bp] = {
+		CSS[flg] = {
 			padding: g/2,
 		};
 
-		mq[flgx + "-" + bp] = {
+		CSS[flgx] = {
 			paddingLeft: g/2,
 			paddingRight: g/2,
 		};
 
-		mq[flgy + "-" + bp] = {
+		CSS[flgy] = {
 			paddingTop: g/2,
 			paddingBottom: g/2,
 		};
 
-		mq[flg + "-" + bp + " > *"] = {
+		CSS[flg + " > *"] = {
 			margin: g/2,
 		};
 
-		mq[flgx + "-" + bp + " > *"] = {
+		CSS[flgx + " > *"] = {
 			marginLeft: g/2,
 			marginRight: g/2,
 		};
 
-		mq[flgy + "-" + bp + " > *"] = {
+		CSS[flgy + " > *"] = {
 			marginTop: g/2,
 			marginBottom: g/2,
 		};
 
 		COLS.forEach(col => {
 			let sels = (
-				flg + " > .fi-" + col + "-" + bp
+				flg + " > .fi-" + col
 				+ "," +
-				flg + "-" + bp + " > .fi-" + col
-				+ "," +
-				flgx + " > .fi-" + col + "-" + bp
-				+ "," +
-				flgx + "-" + bp + " > .fi-" + col
+				flgx + " > .fi-" + col
 			);
 
-			for (let bp2 in WIDTHS) {
-				sels += "," + (flg + "-" + bp + " > .fi-" + col + "-" + bp2);
-				sels += "," + (flgx + "-" + bp + " > .fi-" + col + "-" + bp2);
-			}
-
-			mq[sels] = {
+			CSS[sels] = {
 				width: calc(col/COLS.length, g),
 			};
 		});
 	});
 
-	FLECKS["@media (min-width:" + WIDTHS[bp] + "px)"] = mq;
+	for (let bp in BREAKS) {
+		let mq = {};
+
+		// hidden at bp+
+		mq[".h-" + bp] = {
+			display: "none",
+		};
+
+		// visible at bp+
+		mq[".v-" + bp] = {
+			display: "inline-block",
+		};
+
+		// width snapping to bp
+		mq[".bp"] = {
+			maxWidth: BREAKS[bp] + "px"
+		};
+
+		mq[".fi-" + bp] = {
+			width: "auto",
+		};
+
+		COLS.forEach(col => {
+			mq[".fi-" + col + "-" + bp] = {
+				width: calc(col/COLS.length, 0),
+			};
+		});
+
+		COLS.forEach(col => {
+			mq[".o-" + col + "-" + bp] = {
+				order: col,
+			};
+		});
+
+		GAPS.forEach((g, i) => {
+			let gnum = i + 1;
+			let flg = ".fl.g" + gnum;
+			let flgx = ".fl.g" + gnum + "x";
+			let flgy = ".fl.g" + gnum + "y";
+
+			mq[flg + "-" + bp] = {
+				padding: g/2,
+			};
+
+			mq[flgx + "-" + bp] = {
+				paddingLeft: g/2,
+				paddingRight: g/2,
+			};
+
+			mq[flgy + "-" + bp] = {
+				paddingTop: g/2,
+				paddingBottom: g/2,
+			};
+
+			mq[flg + "-" + bp + " > *"] = {
+				margin: g/2,
+			};
+
+			mq[flgx + "-" + bp + " > *"] = {
+				marginLeft: g/2,
+				marginRight: g/2,
+			};
+
+			mq[flgy + "-" + bp + " > *"] = {
+				marginTop: g/2,
+				marginBottom: g/2,
+			};
+
+			COLS.forEach(col => {
+				let sels = (
+					flg + " > .fi-" + col + "-" + bp
+					+ "," +
+					flg + "-" + bp + " > .fi-" + col
+					+ "," +
+					flgx + " > .fi-" + col + "-" + bp
+					+ "," +
+					flgx + "-" + bp + " > .fi-" + col
+				);
+
+				for (let bp2 in BREAKS) {
+					sels += "," + (flg + "-" + bp + " > .fi-" + col + "-" + bp2);
+					sels += "," + (flgx + "-" + bp + " > .fi-" + col + "-" + bp2);
+				}
+
+				mq[sels] = {
+					width: calc(col/COLS.length, g),
+				};
+			});
+		});
+
+		CSS["@media (min-width:" + BREAKS[bp] + "px)"] = mq;
+	}
+
+	return CSS;
 }
 
-exports.flecks = FLECKS;
+exports.generate = generate;
